750a5463d2eed05bf5d627d9949d85161cf31ef0
Roberto <roherool@hotmail.com>
2022-09-07 20:39:28 -0300

    Modifying the data for each page

--- src/controllers/home.controller.ts
+++
@@ -1,5 +0,0 @@
-import { Request, Response } from "express";
-
-export const HomeController = (req: Request, res: Response) => {
-  res.render("pages/home");
-}
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,14 +1,14 @@
 import { Router } from "express";
 
-import { HomeController } from "../controllers/home.controller";
+import * as HomeController from "../controllers/page.controller";
 import { SearchController } from "../controllers/search.controller";
 
 const router = Router();
 
-router.get("/", HomeController);
-router.get("/cats", HomeController);
-router.get("/dogs", HomeController);
-router.get("/fishes", HomeController);
+router.get("/", HomeController.home);
+router.get("/dogs", HomeController.dogs);
+router.get("/cats", HomeController.cats);
+router.get("/fishes", HomeController.fisher);
 router.get("/search", SearchController);
 
 export default router;
